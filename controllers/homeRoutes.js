013f6c17d4660f0d9e0a67a96b4e4a8aa59c2d88
Jasmine Kay <jasminekay505@gmail.com>
2021-04-04 16:44:20 -0700

    Cleaning up file and adding comments

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,9 +3,9 @@ const router = require('express').Router();
 const { User, Post, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
+//Show all posts on homepage
 router.get('/', async (req, res) => {
     try {
-        // Get all posts and JOIN with user data
         const postData = await Post.findAll({
             include: [
                 {
@@ -15,10 +15,8 @@ router.get('/', async (req, res) => {
             ],
         });
 
-        // Serialize data so the template can read it
         const posts = postData.map((post) => post.get({ plain: true }));
 
-        // Pass serialized data and session flag into template
         res.render('homepage', {
             posts,
             logged_in: req.session.logged_in
@@ -28,6 +26,7 @@ router.get('/', async (req, res) => {
     }
 });
 
+//Single page view with comments
 router.get('/post/:id', async (req, res) => {
     try {
         const postData = await Post.findByPk(req.params.id, {
@@ -41,16 +40,25 @@ router.get('/post/:id', async (req, res) => {
 
         const post = postData.get({ plain: true });
 
+        const commentData = await Comment.findAll({
+            include: { model: User },
+            where: { post_id: post.id }
+        });
+
+        const comments = commentData.map((comment) => comment.get({ plain: true }));
+
         res.render('single-post', {
-            ...post,
+            post,
+            comments,
             logged_in: req.session.logged_in
         });
+
     } catch (err) {
         res.status(500).json(err);
     }
 });
 
-// Use withAuth middleware to prevent access to route
+//Dashboard view for logged_in User
 router.get('/dashboard', withAuth, async (req, res) => {
     try {
         // Find the logged in user based on the session ID
@@ -70,6 +78,7 @@ router.get('/dashboard', withAuth, async (req, res) => {
     }
 });
 
+//Login page
 router.get('/login', (req, res) => {
     // If the user is already logged in, redirect the request to another route
     if (req.session.logged_in) {
@@ -78,6 +87,17 @@ router.get('/login', (req, res) => {
     }
 
     res.render('login');
+});
+
+//Sign up page
+router.get('/signup', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+        res.redirect('/dashboard');
+        return;
+    }
+
+    res.render('signup');
 });
 
 module.exports = router;
