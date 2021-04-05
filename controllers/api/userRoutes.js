d4d82ba18f1e48e12d53fb9700113826da615bfa
Jasmine Kay <jasminekay505@gmail.com>
2021-04-04 17:49:30 -0700

    Sign up and login work!

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,7 +2,7 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-//Create a new user
+//Create a new user and log them in
 router.post('/', async (req, res) => {
     try {
         const userData = await User.create(req.body);
